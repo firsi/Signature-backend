b805a6074794ac6a425e1e950587fe9f3dd11482
Gnakadja Alex <43517259+firsi@users.noreply.github.com>
2019-08-15 11:44:01 +0000

    Delete config.js

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyATTYM1mnPZTrz1k6KPBy16HLWRRy8b-jU",
-    authDomain: "signature-64a52.firebaseapp.com",
-    databaseURL: "https://signature-64a52.firebaseio.com",
-    projectId: "signature-64a52",
-    storageBucket: "signature-64a52.appspot.com",
-    messagingSenderId: "544166436177",
-    appId: "1:544166436177:web:49cf257efd996048"
-  };
